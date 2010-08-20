906d6a84cfb0152da941ff65668d7f82ffecd69b
Scott Ballantyne <scott@ekohe.com>
2010-08-20 15:05:34 +0800

    trying to make it work

--- lib/weibo/request.rb
+++ lib/weibo/request.rb
@@ -23,6 +23,7 @@ module Weibo
 
     def initialize(client, method, path, options={})
       @client, @method, @path, @options = client, method, path, {:mash => true, :query => {:source => Weibo::Config.api_key}}.merge(options)
+      puts @options.inspect
     end
 
 
